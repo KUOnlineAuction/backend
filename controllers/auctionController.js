a876fa1249d0222479a1bbbffe8ac1b6c9bbf653
[Peanut] <peeranat45@gmail.com>
2022-09-27 23:47:25 +0700

    fixed ending_soon

--- controllers/auctionController.js
+++ controllers/auctionController.js
@@ -200,7 +200,7 @@ exports.getSummaryList = catchAsync(async (req, res, next) => {
           coverPicture: "$productDetail.productPicture",
           productName: "$productDetail.productName",
           currentPrice: "$currentPrice",
-          endDate: "$endDate",
+          endDate: { $gt: ["$endDate", Date.now()] },
           //ใช้ได้เฉย
           isWinning: {
             $eq: ["$currentWinnerID", { $toObjectId: decoded.id }],
