4236c4aae0c70e3802fbda988d1cf4f8b22f112e
[Peanut] <peeranat45@gmail.com>
2022-09-27 23:11:35 +0700

    fixed auction

--- models/auctionModel.js
+++ models/auctionModel.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const categoryTypesEnum = [
   "Home Improvement",
   "Jewellery",
-  "Coins, Currentcy, Stamps",
+  "Coins, Currency, Stamps",
   "Watches",
   "Fashion",
   "Arts",
