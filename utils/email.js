051488d8d04784dac3fcbe534d234a3fae570552
[Peanut] <peeranat45@gmail.com>
2022-10-05 12:29:55 +0700

    fixed email

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,5 @@
-const nodemailer = require('nodemailer');
-const nodemailerSendgrid = require('nodemailer-sendgrid')
+const nodemailer = require("nodemailer");
+const nodemailerSendgrid = require("nodemailer-sendgrid");
 
 module.exports = class Email {
   constructor(user, url) {
@@ -10,31 +10,30 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
-      // return nodemailer.createTransport({
-      //   servide: "SendGrid",
-      //   auth: {
-      //     user: process.env.SENDGRID_USERNAME,
-      //     pass: process.env.SENDGRID_PASSWORD
-      //   },
-      //   tls: {rejectUnauthorized: false}
-      // });
-      return nodemailer.createTransport(
-        nodemailerSendgrid({
-          apiKey: process.env.SENDGRID_PASSWORD
-        })
-      )
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   // Sendgrid
+    //   // return nodemailer.createTransport({
+    //   //   servide: "SendGrid",
+    //   //   auth: {
+    //   //     user: process.env.SENDGRID_USERNAME,
+    //   //     pass: process.env.SENDGRID_PASSWORD
+    //   //   },
+    //   //   tls: {rejectUnauthorized: false}
+    //   // });
+    return nodemailer.createTransport(
+      nodemailerSendgrid({
+        apiKey: process.env.SENDGRID_PASSWORD,
+      })
+    );
 
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
+    // return nodemailer.createTransport({
+    //   host: process.env.EMAIL_HOST,
+    //   port: process.env.EMAIL_PORT,
+    //   auth: {
+    //     user: process.env.EMAIL_USERNAME,
+    //     pass: process.env.EMAIL_PASSWORD
+    //   }
+    // });
   }
 
   // Send the actual email
@@ -44,7 +43,7 @@ module.exports = class Email {
       from: this.from,
       to: this.to,
       subject,
-      text: text
+      text: text,
     };
 
     // 3) Create a transport and send email
@@ -52,17 +51,17 @@ module.exports = class Email {
   }
 
   async sendConfirmEmail() {
-    const subject = 'Confirm your account'
+    const subject = "Confirm your account";
     const text = `Hello, thank you for singing up an account for OnlineAuction.com. To continue with your
             registration, please navigate to the URL below to confirm your account validity \n
-            ${this.url} \n`
+            ${this.url} \n`;
     await this.send(subject, text);
   }
 
   async sendPasswordReset() {
-    const subject = 'Password reset'
+    const subject = "Password reset";
     const text = `Please navigate to the URL below to reset your password.\n
-            ${this.url} \n`
+            ${this.url} \n`;
     await this.send(subject, text);
   }
 };
