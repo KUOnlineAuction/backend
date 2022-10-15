963e9b14cd2147f2b0baf6283622c04a5e1601f6
[Peanut] <peeranat45@gmail.com>
2022-10-15 20:57:51 +0700

    sc

--- controllers/scriptController.js
+++ controllers/scriptController.js
@@ -1,16 +1,35 @@
-const catchAsync = require('./../utils/catchAsync');
-const Auction = require('./../models/auctionModel');
-
+const catchAsync = require("./../utils/catchAsync");
+const Auction = require("./../models/auctionModel");
 
 exports.deleteAllUser = catchAsync(async (req, res, next) => {
-    const script = await Auction.deleteMany({});
+  const script = await Auction.deleteMany({});
 
-    if (!script) {
-        return next(new AppError('No Data'), 404)
-    }
+  if (!script) {
+    return next(new AppError("No Data"), 404);
+  }
 
-    res.status(204).json({
-        status: 'success',
-        data: null
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+exports.changeEndDate = catchAsync(async (req, res, next) => {
+  if (req.query.option === "five_minute") {
+    const auction = await Auction.findByIdAndUpdate(req.params.auction_id, {
+      endDate: Date.now() + 5 * 60 * 1000 + 10 * 1000,
     });
+  } else if (req.query.option === "hour") {
+    const auction = await Auction.findByIdAndUpdate(req.params.auction_id, {
+      endDate: Date.now() + 65 * 60 * 1000,
+    });
+  } else if (req.query.option === "day") {
+    const auction = await Auction.findByIdAndUpdate(req.params.auction_id, {
+      endDate: Data.now() + 2 * 24 * 60 * 60 * 1000,
+    });
+  }
+
+  re.status(204).json({
+    status: "success",
+  });
 });
--- routes/scriptRouters.js
+++ routes/scriptRouters.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const scriptController = require('./../controllers/scriptController');
-
+const express = require("express");
+const scriptController = require("./../controllers/scriptController");
 
 const router = express.Router();
 
+router.route("/").delete(scriptController.deleteAllUser);
 
-router.route('/')
-    .delete(scriptController.deleteAllUser)
-
+router
+  .route("/changeEndDate/:auction_id")
+  .patch(scriptController.changeEndDate);
 
 module.exports = router;
